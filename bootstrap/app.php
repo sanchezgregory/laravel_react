bad48b9ba7ef2e29a43bb61dc8a08f30508323a8
Gregory Sanchez <gregory@dev.com>
2024-05-14 16:54:41 -0300

    Regiter and Login by JWTAuth

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,18 +1,40 @@
 <?php
 
+use App\Exceptions\InvalidUserException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        then: function () {
+            Route::middleware('api')
+                ->prefix('api')
+                ->name('api.')
+                ->group(base_path('routes/api.php'));
+        },
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+
+        $exceptions->renderable(function (NotFoundHttpException $e) {
+            return response()->json([
+                'message' => 'Record not found.'
+            ], 404);
+        });
+
+        $exceptions->reportable(function (InvalidUserException $e) {
+            info('User Exception: ' . $e->getMessage());
+        });
+
+        //$exceptions->dontReport(InvalidUserException::class);
+
     })->create();
---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\Auth\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+
+Route::prefix('auth')->group(function() {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    
+});
