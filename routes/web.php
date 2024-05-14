f99d300290b8a19eca0a129aa47df9afbb51e238
Gregory Sanchez <gregory@dev.com>
2024-05-14 13:14:43 -0300

    Laravel and React with Backend config

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::middleware('auth')->group(function(){
+    Route::get('users',[UserController::class, 'index'])->name('users.index');
+});
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
